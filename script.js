5072d8f444cfbb4dec2e53e1418ac4d9b0f0cdca
alex lieberman <alex.lieberman182@gmail.com>
2022-10-01 09:38:29 -0700

    add index, style, and script for title and sub heading

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "An overview of programming skills learned";
 document.querySelector("body").appendChild(h2);
